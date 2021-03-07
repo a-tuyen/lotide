dd995bc109b66310432b28408a75248d8b5f87c4
Amanda Tuyen <atuyen26@gmail.com>
2021-03-07 11:52:29 +0000

    Function to find value in object within object

---
+++ findKey.js
@@ -0,0 +1,52 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+
+const findKey = function(obj, callback) {
+  let keysArr = Object.keys(obj); // returns array of keys ex blue hill, akaleri etc
+  for (let key of keysArr) {  // loop through array of keys
+    // console.log(key);
+    // console.log('key:', callback(key))
+    // console.log('obj key', callback(obj[key]))
+    if (callback(obj[key])) {
+      return key;
+    }
+  } return undefined;
+};
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 5), "noma");
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri");
