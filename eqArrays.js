894beb72fb3813ab53a25bd0aa5c50833175e1b6
Amanda Tuyen <atuyen26@gmail.com>
2021-03-09 09:45:39 +0000

    factored with modules and require

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌ Assertion Failed: ${actual} === ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -23,3 +18,5 @@ assertEqual(eqArrays(['apple', 'orange', 'pear'], ['apple', 'orange', 'pear']), 
 assertEqual(eqArrays(['apple', 'orange', 'pear'], ['apple', 'orange', 'grape']), true);
 assertEqual(eqArrays([], [1, 2, 3]), true);
 assertEqual(eqArrays([], []), true);
+
+module.exports = eqArrays;
