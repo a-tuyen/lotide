35461492ba557705993f164ec5dcc0bf5d2bc9bd
Amanda Tuyen <atuyen26@gmail.com>
2021-03-11 11:38:29 +0000

    created index of all functions

---
+++ index.js
@@ -0,0 +1,26 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const head = require('./head');
+const letterPositions = require('./');
+const map = require('./map');
+const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  assertObjectsEqual: assertObjectsEqual
+};
